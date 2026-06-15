2f2ea49e4b229878148971da5928ebfe742c38dd
Romy Valenzuela <romyvalenzuela311304@gmail.com>
2026-06-15 10:31:10 -0400

    Create paso1.sql

---
+++ entregas/valenzuela_romy/01-veterinaria/paso1.sql
@@ -0,0 +1,8 @@
+INSERT INTO tutores (nombre, telefono) VALUES
+('Pedro Valenzuela', '946572319'),
+('Arturo Gomez', '931805743');
+
+
+UPDATE tutores
+SET telefono = '985674322'
+WHERE nombre = 'Carlos Mendoza';
