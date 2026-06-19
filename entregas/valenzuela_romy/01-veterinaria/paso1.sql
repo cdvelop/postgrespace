3414cf538185dd0b43922c49e14511e14da6d367
Romy Valenzuela <romyvalenzuela311304@gmail.com>
2026-06-19 16:11:09 -0400

    Delete entregas/valenzuela_romy/01-veterinaria/paso1.sql

--- entregas/valenzuela_romy/01-veterinaria/paso1.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO tutores (nombre, telefono) VALUES
-('Pedro Valenzuela', '946572319'),
-('Arturo Gomez', '931805743');
-
-
-UPDATE tutores
-SET telefono = '985674322'
-WHERE nombre = 'Carlos Mendoza';
